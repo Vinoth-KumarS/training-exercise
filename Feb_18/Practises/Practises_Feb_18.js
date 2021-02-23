83feaaa123a6da6547fcae9a2a161e9f527a5ee0
Vinoth2509 <vinoth@provilitiy.com>
2021-02-23 12:30:22 +0530

    Html tables completion

---
+++ Feb_18/Practises/Practises_Feb_18.js
@@ -0,0 +1,33 @@
+var carDetails = {
+    name: ["vinoth", "kuamr", "umar", "saravana"],
+    model: "A6",
+    version: "2021",
+    country: "India"
+}
+
+// console.log(carDetails.name, carDetails.country);
+
+var nameLists = ["vinoth", "kumar", "umar", "saravana"]
+
+// console.log(nameLists[2]);
+// console.log(carDetails.name[2], carDetails.version);
+
+// console.log(nameLists.pop());
+// console.log(nameLists.push("abinesh"));
+// console.log(nameLists)
+// console.log(nameLists.sort())
+// console.log(nameLists.unshift()) 
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(fruits.pop());
+
+// console.log(fruits.push("Pineapple"));
+
+// console.log(fruits.unshift("Lemon"));
+
+// delete fruits[2];
+
+
+fruits.splice(0, 2, "pear", "guava")
+console.log(fruits);
